4318b0b48860978f860f3ebbecc3995cb0942852
Samet Koleoglu <sametkoleoglu@Samet-Mac-mini.local>
2025-11-30 10:15:34 +0300

    Third Commit -> New files and folders were created and errors were fixed.

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -1,14 +1,14 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cnnClassifier import logger
-import joblib
+from src.cnnClassifier import logger
 import json
-import base64
+import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+import base64
 
 
 @ensure_annotations
@@ -25,7 +25,6 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     Returns:
         ConfigBox: ConfigBox type
     """
-
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
@@ -38,25 +37,26 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
 
 @ensure_annotations
-def create_directories(path_to_directories: list, verbose=True) -> None:
-    """create directories
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
 
     Args:
-        path_to_directories (list[Path]): list of path of directories
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
-    for path_to_directory in path_to_directories:
-        os.makedirs(path_to_directory, exist_ok=True)
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"created directory at: {path_to_directory}")
+            logger.info(f"created directory at: {path}")
 
 
 @ensure_annotations
 def save_json(path: Path, data: dict):
-    """save dict as json file
+    """save json data
 
     Args:
         path (Path): path to json file
-        data (dict): data to be saved
+        data (dict): data to be saved in json file
     """
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
@@ -66,18 +66,18 @@ def save_json(path: Path, data: dict):
 
 @ensure_annotations
 def load_json(path: Path) -> ConfigBox:
-    """load json file and return as ConfigBox
+    """load json files data
 
     Args:
         path (Path): path to json file
 
     Returns:
-        ConfigBox: ConfigBox type
+        ConfigBox: data as class attributes instead of dict
     """
-    with open(path, "r") as f:
+    with open(path) as f:
         content = json.load(f)
 
-    logger.info(f"json file loaded from: {path}")
+    logger.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
 
 
@@ -86,10 +86,10 @@ def save_bin(data: Any, path: Path):
     """save binary file
 
     Args:
-        data (Any): data to be saved
+        data (Any): data to be saved as binary
         path (Path): path to binary file
     """
-    joblib.dump(data, path)
+    joblib.dump(value=data, filename=path)
     logger.info(f"binary file saved at: {path}")
 
 
@@ -113,7 +113,7 @@ def get_size(path: Path) -> str:
     """get size in KB
 
     Args:
-        path (Path): path to file
+        path (Path): path of the file
 
     Returns:
         str: size in KB
